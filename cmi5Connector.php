c7c1afa40e511e33d08c496bb001b4c1fe3b9d6b
ADLMeganBohland <megan.bohland.ctr@adlnet.gov>
2022-12-07 15:48:32 -0500

    Url retreival works, now to upload course .zip

--- cmi5Connector.php
+++ cmi5Connector.php
@@ -7,9 +7,14 @@ namespace myWork;
 // @property - 
 class cmi5Connectors{
 
+    //Need to make these global or properties - mb
     public static $tenantName = "";
     public static $tenantId = "";
     public static $bearerToken = "";
+    public static $launchUrl = "";
+
+    //To make a new instance and hold variables??
+  //  global $returnedToken = $connectors->$GLOBALS.$bearerToken;
     
     public function getCreateTenant(){
         return [$this, 'createTenant'];
@@ -122,30 +127,16 @@ class cmi5Connectors{
         echo "actorname is {$actor} and the home URL is {$homeUrl}, now returnURL is {$retUrl}, the token is {$token}, and its going to {$reqUrl}";
         echo "<br>";
 
-        /**
-         *  //the body of the request must be made as array first
-        *$data = array(
-        *   'tenantId' => $id,
-        *  'audience' => $tokenUser
-        *);
-         */
         //the body of the request must be made as array first
-        $data1 = array(
+        $data = array(
             'actor' => array (
                 'account' => array(
-                    "homePage:" => $homeUrl,
-                    "name:" => $actor,
+                    "homePage" => $homeUrl,
+                    "name" => $actor,
                 )
             ),
             'returnUrl' => $retUrl
         );
-
-        $data2 = json_encode($data1, JSON_UNESCAPED_SLASHES);
-
-        echo "<br>";
-        echo "Ok, how are we doing, is the body correct?  ";  
-        echo"$data2";
-        echo "<br>";
     
         // use key 'http' even if you send the request to https://...
         //There can be multiple headers but as an array under the ONE header
@@ -158,71 +149,19 @@ class cmi5Connectors{
                 'header' => array("Authorization: Bearer ". $token,  
                     "Content-Type: application/json\r\n" .
                     "Accept: application/json\r\n"),
-                'content' => json_encode($data1, JSON_UNESCAPED_SLASHES)
+                'content' => json_encode($data, JSON_UNESCAPED_SLASHES)
 
             )
-        )
-            ;
-
-        
-        echo "<br>";
-        echo" huh what about error " . json_last_error();
-
-        echo "<br>";
-        echo"Welll what is content??";
-        var_dump($options);
-        echo "<br>";
-
-
+        );
 
         //the options are here placed into a stream to be sent
-       $context  = stream_context_create(($options));
-        echo "<br>";
-       echo "And the context is " . $context;
-        echo "<br>";
+        $context  = stream_context_create(($options));
 
-        /*
-        //lets see if this brings back a cERTAIN error
-        $context = stream_context_create(array(
-            'http' => array('ignore_errors' => true,
-            $options
-        )));
-        */
-        //$result = file_get_contents('http://your/url', false, $context);
-        echo "<br>";
-        echo"Welll, and where is it going??? {$reqUrl}";
-        echo "<br>";
-
-        //$reqUrl = urlencode($reqUrl);
         //sends the stream to the specified URL and stores results (the false is use_include_path, which we dont want in this case, we want to go to the url)
-        $result = file_get_contents( $reqUrl, false, $context );
-    /*
-        $fp = fopen($reqUrl, 'r', false, $context);
-        fpassthru($fp);
-        fclose($fp);
-*/
-        echo "<br> url RETRIEVED. URL is $result";
-        echo "<br>";
-        var_dump(json_decode($result, true));
-        //return response
-
-        echo"<br>";
-        echo"About to dump http_res_headers: ";
-        echo"<br>";
-
-        //maybe this will shed light?
-        if ($result === FALSE) {
-            var_dump($http_response_header);
-        }
-            echo"<br>";
-
-            echo"<br>";
-        echo "where any headers sent?";
-        echo "<br>";
-        var_dump(headers_sent());
-
+        $launchResponse = file_get_contents( $reqUrl, false, $context );
         
-       // return $result;
+        //return response
+        return $launchResponse;
     }
         
      
@@ -255,7 +194,6 @@ class cmi5Connectors{
         );
         //the options are here placed into a stream to be sent
         $context  = stream_context_create($options);
-        
     
         //sends the stream to the specified URL and stores results (the false is use_include_path, which we dont want in this case, we want to go to the url)
         $result = file_get_contents( $url, false, $context );
--- tenant.php
+++ tenant.php
@@ -2,6 +2,14 @@
 namespace myWork;
 require_once("./cmi5Connector.php");
 
+
+
+buttonPushed();
+
+//Does the whole thing need to be a func to hold global ariableS?
+
+function buttonPushed() {
+
 //to hold variables gather from forms
 $newTenantName = htmlspecialchars($_POST["textboxForNewName"] ?? "", ENT_QUOTES);
 $firstUserName = htmlspecialchars($_POST["textboxForUsername1"] ?? "", ENT_QUOTES);
@@ -21,11 +29,11 @@ $returnUrl = htmlspecialchars($_POST["textboxForReturnUrl"] ?? "", ENT_QUOTES);
 //to bring in functions
 $connectors = new cmi5Connectors;
 
-
-//To hold variables overall, aka after manipulation from functions
+  //To hold variables overall, aka after manipulation from functions
 $returnedTenName = "";
 $returnedTenId = "";
-$returnedToken = "";
+//$//returnedToken = "";
+global $bearerToken;
 
 //Check which button was pushed
 if (isset($_POST['Register'])) {
@@ -49,7 +57,8 @@ if (isset($_POST['Register'])) {
         echo"returned id is $returnedTenId";
 
 }elseif (isset($_POST['GetToken'])) {
-    
+
+  
     echo"Get Token button pushed";
     echo"<div class=\"feedback\">Username: $userName<br>Audience: $audience</div><br>Tenent ID: $tenantId</div><br>Password: $password</div><br>URL: $url</div>";
 
@@ -58,55 +67,55 @@ if (isset($_POST['Register'])) {
     //will retreive tenants bearer token
   $returnedToken = $retrieveToken($url, $userName, $password, $audience, $tenantId);
 
-}elseif (isset($_POST['GetURL'])) {
- 
-  echo"Get Token button pushed";
-  echo"<div class=\"feedback\">Actor name: $actorName<br>Homepage URL: $homepage</div><br>Return URL: $returnUrl</div>";
+  $bearerToken = $returnedToken;
+
+
+} elseif (isset($_POST['GetURL'])) {
+
+  echo "Get Token button pushed";
+  echo "<div class=\"feedback\">Actor name: $actorName<br>Homepage URL: $homepage</div><br>Return URL: $returnUrl</div>";
+  
   //$actorName = "your";
   $courseId = "9";
   $auIndex = "0";
-  
+
   //URL to send request to
   $url = "http://localhost:63398/api/v1/course/{$courseId}/launch-url/{$auIndex}";
-  echo"<br>";
-  echo "is the url being built correctly???   {$url}";
+
 
   echo "<br>";
-  echo "Can it see returned TOken here?   + $returnedToken";
+  echo "Can it see returned TOken here?" .   $_POST['bearerToken'];
   echo "<br>";
-//No it CANN>OT because we keep resubmitting form so lea=ts hardcode for know to practice
-  
+  //No it CANN>OT because we keep resubmitting form so lea=ts hardcode for know to practice
+
   $token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJ1cm46Y2F0YXB1bHQ6cGxheWVyIiwiYXVkIjoidXJuOmNhdGFwdWx0Ok1vb2RsZSIsInN1YiI6MywianRpIjoiM2FlZDliNDEtYjk4Yy00NmJiLTkzYmUtMmM5ZTRiMzhlZWUxIiwiaWF0IjoxNjcwMjYwNzE2fQ.8w0QiKNE4RMOyLMcktfX_fruhS1i-kfqlfaWO2WYJP4";
-  
-  
+
+
   $retrieveUrl = $connectors->getRetrieveUrl();
   $result = $retrieveUrl($actorName, $homepage, $returnUrl, $url, $token);
-  
 
+  if ($result === FALSE) {
+    echo "Something went wrong!";
+    echo "<br>";
+    var_dump($_SERVER);
+  } else {
 
+    $launchResponse = json_decode($result, true);
 
-  echo"<br>";
-  echo "It is working here is result ++ {$result}";
+    $sessionId = $launchResponse['id'];
+    $launchMethod = $launchResponse['launchMethod'];
+    $launchUrl = $launchResponse['url'];
 
-  if ($result === FALSE) 
-  { echo"Something went wrong!";
-      echo"<br>";
-      var_dump($_SERVER);
   }
-  else{
-  echo "Tenant created. Response: is $result";
-  //var_dump(json_decode($result, true));
-  }
-  //decode returned response into array
-  //$returnedInfo = json_decode($result, true);
-  
-  //Return an array with tenant name and info
-  //return $returnedInfo;
+
+  echo "<br>";
+  echo "<br>";
+
+  echo "<br>";
+  echo "Let's make sure the decode worked. The returned session id is {$sessionId}, and the launch method is {$launchMethod}, and the long await URL is {$launchUrl}";
+
+  echo "<br>";
+  echo "<br>";
+};
 }
-
-echo "<br>";
-echo "<br>";
-
-echo "<br>";
-echo "<br>";
 ?>
